8490109ba25e7876ed97401a6e8b7af0f63e0fe4
Reflection <firesoft1212@gmail.com>
2024-05-11 22:43:55 +0900

    Add bounce animation

--- CondorDock/Animation.cs
+++ CondorDock/Animation.cs
@@ -11,6 +11,25 @@ namespace CondorDock
 {
     internal class Animation
     {
-        
+        /// <summary>
+        /// Run a bounce animation on a control
+        /// </summary>
+        /// <param name="control">The control to animate</param>
+        /// <param name="from">Relative start position of the control animation</param>
+        /// <param name="to">Relative end position of the control animation</param>
+        /// <param name="duration">Duration of the animation in seconds</param>
+        public static void RunBounceAnimation(Control control, double from, double to, double duration)
+        {
+            DoubleAnimation dblAni = new(from, to, TimeSpan.FromSeconds(duration))
+            {
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            TranslateTransform trans = new();
+            control.RenderTransform = trans;
+
+            trans.BeginAnimation(TranslateTransform.YProperty, dblAni);
+        }
     }
 }
