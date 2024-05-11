52b609dcf2a07e68f7be0845526bdb460aa5868e
Reflection <firesoft1212@gmail.com>
2024-05-11 22:43:33 +0900

    초기 커밋.

---
+++ CondorDock/Animation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+
+namespace CondorDock
+{
+    internal class Animation
+    {
+        
+    }
+}
